d79ac1f00b77c76f9a6d4bff5f3b3cbb2e8d1d5d
Kartik saxena <kartiksaxena@kartiks-mbp.home>
2023-08-27 16:28:31 -0400

    build: test cases for stripe API

--- shoppers_mart/payments/tests.py
+++ shoppers_mart/payments/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.conf import settings
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from unittest.mock import patch
+import stripe
 
-# Create your tests here.
+class StripeCheckoutViewTest(APITestCase):
+
+    @patch('stripe.checkout.Session.create')
+    def test_successful_checkout(self, mock_create_session):
+        # Mock Stripe API response
+        mock_create_session.return_value = {
+            'id': 'test_session_id',
+            'url': 'https://example.com/session/test_session_id',
+        }
+
+        url = reverse('stripe-checkout')
+        response = self.client.post(url)
+
+        print(response.content)  # Print the response content for debugging
+
+        # Check that the response status is 302 (redirect)
+        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
+
+        # Check that the response redirected to the expected URL
+        self.assertEqual(response.url, 'https://example.com/session/test_session_id')
+
+
+    @patch('stripe.checkout.Session.create')
+    def test_failed_checkout(self, mock_create_session):
+        # Mock Stripe API response for a failed checkout
+        mock_create_session.side_effect = stripe.error.StripeError(message='Stripe API error')
+
+        url = reverse('stripe-checkout')
+        response = self.client.post(url)
+
+        # Check that the response status is 500 (internal server error)
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        self.assertEqual(response.data['error'], 'Sorry, something went wrong!')
